6b8177a75a13561c7c3d6201ad55cebab4020abe
ZerubbabelT <thundervoltatbolt@gmail.com>
2025-06-04 05:47:06 +0300

    feat: rename ReactQueryProvider to TanstackQueryProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/layout/NavBar";
-import ReactQueryProvider from "@/lib/react-query-provider";
+import TanstackQueryProvider from "@/lib/tanstack-query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,10 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ReactQueryProvider>
+        <TanstackQueryProvider>
           <NavBar />
           {children}
-        </ReactQueryProvider>
+        </TanstackQueryProvider>
       </body>
     </html>
   );
--- lib/tanstack-query-provider.tsx
+++ lib/tanstack-query-provider.tsx
@@ -2,7 +2,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
 
-export default function ReactQueryProvider({
+export default function TanstackQueryProvider({
   children,
 }: {
   children: ReactNode;
